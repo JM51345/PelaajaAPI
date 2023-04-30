3b49e7779e7aba529b40ed830d269a774bcafa67
test <122447645+JM51345@users.noreply.github.com>
2023-04-30 22:44:49 +0300

    update main.py

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from .router import events, players
 
 from .database import models
@@ -11,3 +12,11 @@ app = FastAPI()
 app.include_router(events.router)
 app.include_router(players.router)
 
+origins = ['*']
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=['*'],
+    allow_headers=['*'])
